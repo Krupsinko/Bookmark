4f0cbd3328a610ee8f2d2176d638ac3c12010a04
Krupsinko <matikurpanik@gmail.com>
2025-10-22 19:21:40 +0200

    Zaktualizuj obraz kontenera w pliku .actrc oraz sformatuj kod w database.py i dodaj plik requirementx.txt z zależnościami

--- app/db/database.py
+++ app/db/database.py
@@ -9,9 +9,14 @@ DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_async_engine(
     DATABASE_URL,
-    echo=True)
+    echo=True
+    )
 
-SessionLocal = async_sessionmaker(autoflush=False, bind=engine, class_=AsyncSession)
+SessionLocal = async_sessionmaker(
+    autoflush=False,
+    bind=engine,
+    class_=AsyncSession
+    )
 
 class Base(DeclarativeBase):
     pass
